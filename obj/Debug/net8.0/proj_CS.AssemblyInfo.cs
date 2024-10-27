cc6730f6f0e2db2c441e5592612245eff45ec4ac
Charlie-XD <xdcharlie67@gmail.com>
2024-10-27 23:11:51 +0600

    fixed

--- obj/Debug/net8.0/proj_CS.AssemblyInfo.cs
+++ obj/Debug/net8.0/proj_CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj_CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a343e2bec5a4ebab704cb21b9c462e63f5b08e")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj_CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj_CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
